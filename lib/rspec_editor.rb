1b7c0c7dc8366a83fafd2eb5f244aeb407ea048b
Kurt Stephens <ks.github@kurtstephens.com>
2014-12-22 17:04:10 -0600

    Load the Formatter.

--- lib/rspec_editor.rb
+++ lib/rspec_editor.rb
@@ -1,6 +1,9 @@
 require "rspec_editor/version"
 
 module RspecEditor
-  # Your code goes here...
+  def self.root_dir
+    File.expand_path('../../', __FILE__)
+  end
 end
 require 'rspec_editor/editor'
+require 'rspec_editor/formatter'
